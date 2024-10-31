1fff01c33d94feab5120a16febdad018c25a2445
SarilTest <sarilpriappan@gmail.com>
2024-10-31 18:31:46 +0000

    removed unused import

--- src/test/java/project/Config.java
+++ src/test/java/project/Config.java
@@ -1,13 +1,11 @@
 package project;
 
-import org.testng.annotations.DataProvider;
 
 public class Config {
     public static final String BASE_URL = "https://www.saucedemo.com/v1/index.html";
     public static final String STANDARD_USER = "standard_user";
     public static final String PASSWORD = "secret_sauce";
 
-    // Additional configurations if needed
     
  
 }
